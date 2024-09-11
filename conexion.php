35023d70d59be79f78f30eca5d5dd09199e75fbe
Roger Rodriguez <rogeralexster@gmail.com>
2024-09-11 17:13:13 -0300

    Contraseña fragil

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "vncadmin", "club_beisbol");
+$mysqli = new mysqli("localhost", "root", "12345", "club_beisbol");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
